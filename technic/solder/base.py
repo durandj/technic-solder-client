0353f1fc440ec646f39b2e19143dd37c24608c3d
Cadyyan <cadyyan@avalon>
2014-12-30 13:16:32 -0600

    Initial commit

---
+++ technic/solder/base.py
@@ -0,0 +1,60 @@
+import requests
+import requests.exceptions
+import urlparse
+
+from .exceptions import SolderAPIError
+
+class SolderServer(object):
+	def __init__(self, solder_url):
+		self.solder_url = solder_url
+
+		self._modpacks = None
+
+	def get_modpack_info(self, slug):
+		return self._request(
+			'/api/modpack/{slug}',
+			'GET',
+			slug = slug,
+		)
+
+	@property
+	def server_info(self):
+		info = self._request(
+			'/api',
+			'GET',
+		)
+
+		return (info['version'], info['stream'])
+
+	@property
+	def modpacks(self):
+		if not self._modpacks:
+			self._modpacks = self._request('/api/modpack', 'GET')['modpacks']
+
+		return self._modpacks
+
+	def _request(self, url, method, **kwargs):
+		url_parts = urlparse.urlparse(self.solder_url)
+
+		url = urlparse.urlunparse(
+			(
+				url_parts.scheme,
+				url_parts.netloc,
+				url.format(**kwargs),
+				'',
+				'',
+				'',
+			)
+		)
+
+		resp = requests.request(method, url)
+
+		if not resp.status_code == 200:
+			raise SolderAPIError('API connection error ({})'.format(resp.status_code))
+
+		json_resp = resp.json()
+		if 'error' in json_resp:
+			raise SolderAPIError(json_resp['error'])
+
+		return json_resp
+
